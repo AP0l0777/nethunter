d79fb8bb21a86bf924dce5421bdd723c21e9d67d
AP0l0777 <105818448+AP0l0777@users.noreply.github.com>
2022-08-19 14:35:49 -0300

    Update InstallKali.sh

--- KaliLinux/InstallKali.sh
+++ KaliLinux/InstallKali.sh
@@ -3,6 +3,7 @@ apt update && apt update -y
 apt update
 mv InstallKali.sh $HOME
 mv k.sh $HOME
+cd
 apt install wget
 wget -O install-nethunter-termux https://offs.ec/2MceZWr
 chmod 777 install-nethunter-termux
