2cb33aa811a6beab7421024208fc2411c75e7a0c
AP0l0777 <105818448+AP0l0777@users.noreply.github.com>
2022-08-19 14:02:56 -0300

    Update k.sh

--- KaliLinux/k.sh
+++ KaliLinux/k.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd
 cd storage/shared/download
-mv kalifs.armhf-full.tar.xr $HOME
-cd 
+mv kalifs.armhf-full.tar.xr /data/data/com.termux/files/home/nethunter/KaliLinux
+cd /data/data/com.termux/files/home/nethunter/KaliLinux
 ./install-nethunter-termux
+nh
