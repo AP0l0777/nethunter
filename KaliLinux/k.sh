ae03fdede5a073b0e30ac2dbd2799f892bd8b11c
AP0l0777 <105818448+AP0l0777@users.noreply.github.com>
2022-08-19 15:49:55 -0300

    Update k.sh

--- KaliLinux/k.sh
+++ KaliLinux/k.sh
@@ -4,5 +4,9 @@ cd storage/shared/download
 mv kalifs.armhf-full.tar.xr /data/data/com.termux/files/home
 mv k.sh /data/data/com.termux/files/home
 cd /data/data/com.termux/files/home
+clear
+echo " DIGITE (N) PARA: [?] Existing image file found. Delete and download a new one? [y/N]"
+sleep 3s
+echo " DIGITE (Y) PARA : [?] Delete downloaded rootfs file ? [y/N]"
+sleep 3s
 ./install-nethunter-termux
-nh
