451c7cedd215a3aa6ebd22a3cb3070307dea6abe
AP0l0777 <105818448+AP0l0777@users.noreply.github.com>
2022-08-19 13:48:10 -0300

    Update InstallKali.sh

--- KaliLinux/InstallKali.sh
+++ KaliLinux/InstallKali.sh
@@ -4,4 +4,13 @@ apt update
 apt install wget
 wget -O install-nethunter-termux https://offs.ec/2MceZWr
 chmod 777 install-nethunter-termux
+chmod 777 ./k.sh
 ./install-nethunter-termux
+clear
+echo " permita o acesso"
+termux-setup-storage
+echo "agora voce irar baixar a imagem do kali e mover para a pasta Home"
+echo " aguarde abrindo navegador, faça o download manualmente no seu navegador!"
+echo "Aguarde o Download ser concluido e execute  ./k.sh"
+sleep 3s
+termux-open https://kali.downloa/nethunter-images/current/rootfs/kalifs-armhf.tar.xz
