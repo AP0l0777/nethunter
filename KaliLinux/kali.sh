42630d6c61ae06848e394207188ac26edf61a1c3
AP0l0777 <105818448+AP0l0777@users.noreply.github.com>
2022-08-21 01:08:08 -0300

    Create kali.sh

---
+++ KaliLinux/kali.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+apt update && apt update -y apt update
+echo "Permita Acesso! "
+sleep 2s
+termux-setup-storage
+pkg install wget -Y
+clear
+ls
+cd storage/shared/download
+echo " Aguarde O Download "
+sleep 2s
+wget https://kali.download/nethunter-images/current/rootfs/kalifs-armhf-full.tar.xz
+clear
+echo " Download Concluido! "
+mv kalifs-armhf-full.tar.xz /data/data/com.termux/files/home
+cd /data/data/com.termux/nethunter/KaliLinux
+mv InstallKali.sh /data/data/com.termux/files/home
+mv k.sh /data/data/com.termux/files/home
+chmod 777 k.sh
+chmod 777 InstallKali.sh
+./k.sh
