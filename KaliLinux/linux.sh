d52b032a114f90fcc27b529cb1ca2bc15925219e
AP0l0777 <105818448+AP0l0777@users.noreply.github.com>
2022-08-21 02:42:42 -0300

    Update linux.sh

--- KaliLinux/linux.sh
+++ KaliLinux/linux.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 clear
 echo " Download Concluido! "
+cd $HOME
 cd storage/shared/download
 mv kalifs-armhf-full.tar.xz /data/data/com.termux/files/home
 cd $HOME
-cd /data/data/com.termux/nethunter/KaliLinux
-mv /data/data/com.termux/nethunter/KaliLinux/InstallKali.sh $HOME
-mv /data/data/com.termux/nethunter/KaliLinux/k.sh $HOME
+cd nethunter/KaliLinux
+mv InstallKali.sh $HOME
+mv k.sh $HOME
 cd $HOME
 chmod 777 k.sh
 chmod 777 InstallKali.sh
