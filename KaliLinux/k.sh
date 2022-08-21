8240ed1ce0a61eb046b75bc09641667778856c8d
AP0l0777 <105818448+AP0l0777@users.noreply.github.com>
2022-08-21 00:46:54 -0300

    Update k.sh

--- KaliLinux/k.sh
+++ KaliLinux/k.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-cd /data/data/com.termux/files/home
-cd storage/shared/download
-mv kalifs.armhf-full.tar.xr /data/data/com.termux/files/home
-mv k.sh /data/data/com.termux/files/home
-cd /data/data/com.termux/files/home
+
 clear
-echo " DIGITE (N) PARA: [?] Existing image file found. Delete and download a new one? [y/N]"
+echo " DIGITE ( N ) PARA: [?] Existing image file found. Delete and download a new one? [y/N]"
 sleep 3s
-echo " DIGITE (Y) PARA : [?] Delete downloaded rootfs file ? [y/N]"
+echo " DIGITE ( Y ) PARA : [?] Delete downloaded rootfs file ? [y/N]"
+echo " esse processo pode demorar dependendo da sua intenet "
+echo "que tal ir se hidratar !"
 sleep 3s
-./install-nethunter-termux
+./InstallKali.sh
