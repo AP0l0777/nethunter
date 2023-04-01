3c1b9c5532ce981613d343b4e224a0a6f7e7d8ac
AP0l0777 <105818448+AP0l0777@users.noreply.github.com>
2023-03-31 22:49:07 -0300

    Update InstallKali.sh

--- KaliLinux/InstallKali.sh
+++ KaliLinux/InstallKali.sh
@@ -3,6 +3,6 @@ wget -O install-nethunter-termux https://offs.ec/2MceZWr
 chmod 777 install-nethunter-termux
 ./install-nethunter-termux
 clear
-echo " Script Finalizado!"
-sleep 1s
+echo -e " \e[1:33;36m KALI LINUX \e[0m INSTALADO"
+sleep 2s
 nh
